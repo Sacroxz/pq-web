beba5126fb6837af7a5bf0c74cfff9dcaf6f9740
Sacroxz <ed.navarrol@duocuc.cl>
2022-11-21 01:22:25 -0300

    Added home route

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html")
+
+@app.route("/burger")
+def burger():
+    return "<p>hola soy un boton<p>"
+
+@app.route("/prueba")
+def prueba():
+    return render_template("prueba.html")
+
+@app.route("/prueba2")
+def prueba2():
+    return render_template("prueba2.html")
+    
+@app.route("/prueba3")
+def prueba3():
+    return render_template("prueba3.html")

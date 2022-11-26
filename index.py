3aa0edeb90f36da2cc846f4cff16e24959bac7aa
Sacroxz <ed.navarrol@duocuc.cl>
2022-11-26 13:38:02 -0300

    testing login

--- index.py
+++ index.py
@@ -1,19 +1,23 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 import oracledb
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
-# connection = cx_Oracle.connect(user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'), dsn="ehuurlu4scvu47zv_low", encoding="UTF-8")
-# connection = oracledb.connect(user="admin", password="x52JjUGZrTa8VZw", dsn="ehuurlu4scvu47zv_high", config_dir="opt/config", wallet_location="opt/config", wallet_password="x52JjUGZrTa8VZw", encoding="UTF-8")
-connection = oracledb.connect(user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'), dsn=os.environ.get('DB_DSN'), config_dir="opt/config", wallet_location="opt/config", wallet_password=os.environ.get('DB_PASSWORD'), encoding="UTF-8")
+
+# connection = cx_Oracle.connet(user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'), dsn="ehuurlu4scvu47zv_low", encoding="UTF-8")
+connection = oracledb.connect(user="admin", password="x52JjUGZrTa8VZw", dsn="ehuurlu4scvu47zv_high", config_dir="opt/config", wallet_location="opt/config", wallet_password="x52JjUGZrTa8VZw", encoding="UTF-8")
+# connection = oracledb.connect(user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'), dsn=os.environ.get('DB_DSN'), config_dir="opt/config", wallet_location="opt/config", wallet_password=os.environ.get('DB_PASSWORD'), encoding="UTF-8")
 
 print("Database version:", connection.version)
 
 @app.route("/")
 def home():
-    data = connection.version
-    return render_template("home.html", data=data)
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM PERSONS")
+    rows = cur.fetchall()
+    return render_template("home.html", rows=rows)
 
 @app.route('/prequirurgico')
 def prequirurgico():
@@ -37,16 +41,30 @@ def postquirurgico():
             {'name': 'Fase Postquirurgica', 'url': '/postquirurgico'}]
     return render_template("postquirurgico.html", links=links)
 
-@app.route("/login")
+@app.route('/login')
 def login():
     links = [
-        {'name': 'Home', 'url': '/'}, 
-        {'name': 'Iniciar Sesion', 'url': '/login'}]
-    return render_template("login.html", links=links)
-# TODO: Add post logic.
-# @app.route("/post", methods=["POST"])
-# def post_cuidados():
-#     return render_template("post.html")
+            {'name': 'Home', 'url': '/'}, 
+            {'name': 'Iniciar Sesion', 'url': '/login'}]
+    return render_template('login.html', links=links)
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    cur = connection.cursor()
+    cur = cur.execute("SELECT * FROM USUARIOS WHERE correo = :email AND contrasena = :password", email=email, password=password)
+    user = cur.fetchone()
+    if not user:
+        flash('Tu correo o contraseña son incorrectos')
+        return redirect(url_for('login'))
+    return redirect(url_for('home'))
+
+
+@app.route('/logout')
+def logout():
+    return 'Logout'
+
 
 if __name__ == '__main__':
     app.run()

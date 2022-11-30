808b170c05041282a346d51a63f7a598d10c0fc9
Sacroxz <ed.navarrol@duocuc.cl>
2022-11-29 23:09:48 -0300

    Changed routing and forms format

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 from index import db
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, DateField, TimeField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
+from wtforms.validators import InputRequired, Length
 
 class LoginForm(FlaskForm):
   mail = StringField('mail', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Mail"})
@@ -15,11 +15,11 @@ class RutForm(FlaskForm):
   
 
 class EvaluacionPacienteForm(FlaskForm):
-  temperatura = StringField('temperatura', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Temperatura"})
-  pulso = StringField('pulso', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Pulso"})
-  presion_arterial = StringField('presion_arterial', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Presion Arterial"})
-  frecuencia_cardiaca = StringField('frecuencia_cardiaca', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Frecuencia Cardiaca"})
-  frecuencia_respiratoria = StringField('frecuencia_respiratoria', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Frecuencia Respiratoria"})
+  rut_paciente = StringField('rut_paciente', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Rut"})
+  temperatura = StringField('temperatura', validators=[InputRequired()], render_kw={"placeholder": "Temperatura"})
+  presion_arterial = StringField('presion_arterial', validators=[InputRequired()], render_kw={"placeholder": "Presion Arterial"})
+  frecuencia_cardiaca = StringField('frecuencia_cardiaca', validators=[InputRequired()], render_kw={"placeholder": "Frecuencia Cardiaca"})
+  frecuencia_respiratoria = StringField('frecuencia_respiratoria', validators=[InputRequired()], render_kw={"placeholder": "Frecuencia Respiratoria"})
   examen_fisico = TextAreaField('examen_fisico', validators=[InputRequired(), Length(min=4, max=200)], render_kw={"placeholder": "Examen Fisico"})
   zona_operacion = TextAreaField('zona_operacion', validators=[InputRequired(), Length(min=4, max=200)], render_kw={"placeholder": "Zona Operacion"})
   observacion = TextAreaField('observacion', validators=[Length(min=4, max=200)], render_kw={"placeholder": "Observacion"})
@@ -46,21 +46,3 @@ class CoordinarTrasladoForm(FlaskForm):
   rut_conductor = StringField('rut_conductor', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Rut Conductor"})
   direccion = StringField('direccion', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Direccion"})
   submit = SubmitField("Guardar")
-
-  
-  # class Usuario(db.Model, UserMixin):
-#   def __init__(self, rut, nombre, apellido, telefono, mail, contrasena):
-#     self.rut = rut
-#     self.nombre = nombre
-#     self.apellido = apellido
-#     self.telefono = telefono
-#     self.mail = mail
-#     self.contrasena = contrasena
-
-#   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#   rut = db.Column(db.String(20), unique=True, nullable=False)
-#   nombre = db.Column(db.String(20), nullable=False)
-#   apellido = db.Column(db.String(20), nullable=False)
-#   telefono = db.Column(db.String(20), nullable=False)
-#   mail = db.Column(db.String(100), unique=True, nullable=False)
-#   contrasena = db.Column(db.String(100), nullable=False)

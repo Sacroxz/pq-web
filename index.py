8623906c0cc0ddd852fcdc5953242bd58977e3e3
Sacroxz <ed.navarrol@duocuc.cl>
2022-11-29 23:10:36 -0300

    Added new routes and models

--- index.py
+++ index.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, redirect, url_for, flash, session, request
 from flask_migrate import Migrate
 from flask_login import login_user, LoginManager, login_required, logout_user, current_user, UserMixin
-from datetime import datetime
 
 oracledb.version = "8.3.0"
 sys.modules["cx_Oracle"] = oracledb
@@ -16,7 +15,6 @@ app = Flask(__name__)
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_DSN = os.environ.get('DB_DSN')
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'oracle://{DB_USER}:{DB_PASSWORD}@{DB_DSN}/?config_dir=opt/config&wallet_location=opt/config&wallet_password={DB_PASSWORD}'
 app.config['SECRET_KEY'] = 'thisisascretkey'
@@ -70,21 +68,31 @@ class Conductor(db.Model):
     traslado = db.relationship('Traslado', backref='conductor', lazy=True)
 class Paciente(db.Model):
     __tablename__ = 'paciente'
-    rut = db.Column(db.String(20), primary_key=True)
+    id = db.Column(db.Integer, autoincrement=True)
+    rut = db.Column(db.String(20), primary_key=True, nullable=False)
     nombre = db.Column(db.String(30), nullable=False)
     apellido = db.Column(db.String(30), nullable=False)
+    sexo = db.Column(db.Boolean, nullable=False)
     telefono = db.Column(db.Integer, nullable=False)
     correo = db.Column(db.String(100), nullable=False)
     direccion = db.Column(db.String(100), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
     traslado = db.relationship('Traslado', backref='paciente', lazy=True)
-
-
-
-
+    evaluacion = db.relationship('Evaluacion', backref='paciente', lazy=True)
+class Evaluacion(db.Model):
+    __tablename__ = 'evaluacion'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    rut_paciente = db.Column(db.String(20), db.ForeignKey('paciente.rut'), nullable=False)
+    temperatura = db.Column(db.Integer, nullable=False)
+    presion_arterial = db.Column(db.Integer, nullable=False)
+    frecuencia_cardiaca = db.Column(db.Integer, nullable=False)
+    frecuencia_respiratoria = db.Column(db.Integer, nullable=False)
+    examen_fisico = db.Column(db.String(100), nullable=False)
+    zona_operacion = db.Column(db.String(100), nullable=False)
+    observacion = db.Column(db.String(100), nullable=False)
+    fecha = db.Column(db.Date)
 
 migrate = Migrate(app, db, command="migrate")
-# bcrypt = Bcrypt(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -96,7 +104,7 @@ def load_user(user_id):
 
 @app.route("/")
 def home():
-    
+    print("HEY")
     # print('usuarios:', Usuario.query.all())
     return render_template("home.html")
 
@@ -164,26 +172,46 @@ def logout():
 def prueba():
 	return render_template("prueba2.html")
 
-@app.route("/evaluacion_paciente")
+@app.route("/postquirurgico/evaluacion_paciente", methods=['GET', 'POST'])
 @login_required
 def evaluacion_paciente():
     links = [
             {'name': 'Home', 'url': '/'}, 
             {'name': 'Fase Postquirurgica', 'url': '/postquirurgico'},
-            {'name': 'Evaluacion Paciente', 'url': '/evaluacion_paciente'}]
-    from models import RutForm, EvaluacionPacienteForm
-    rut_form = RutForm()
+            {'name': 'Evaluacion Paciente', 'url': '/postquirurgico/evaluacion_paciente'}]
+    from models import EvaluacionPacienteForm
     evaluacion_paciente_form = EvaluacionPacienteForm()
-    return render_template("evaluacion_paciente.html", 
-        rut_form=rut_form, evaluacion_paciente_form=evaluacion_paciente_form, links=links)
+    if request.method == 'POST':
+        print(evaluacion_paciente_form.validate_on_submit())
+    if evaluacion_paciente_form.validate_on_submit():
+        paciente = Paciente.query.filter_by(rut=request.form['rut_paciente']).first()
+        if paciente:
+            evaluacion = Evaluacion(
+                rut_paciente=request.form['rut_paciente'],
+                temperatura=request.form['temperatura'],
+                presion_arterial=request.form['presion_arterial'],
+                frecuencia_cardiaca=request.form['frecuencia_cardiaca'],
+                frecuencia_respiratoria=request.form['frecuencia_respiratoria'],
+                examen_fisico=request.form['examen_fisico'],
+                zona_operacion=request.form['zona_operacion'],
+                observacion=request.form['observacion']
+            )
+            db.session.add(evaluacion)
+            db.session.commit()
+            flash('Evaluacion agregada correctamente')
+            return redirect(url_for('postquirurgico'))
+        else:
+            flash('Paciente no encontrado')
+            return redirect(url_for('evaluacion_paciente'))
+    return render_template("evaluacion_paciente.html", evaluacion_paciente_form=evaluacion_paciente_form, links=links)
 
-@app.route("/evaluacion_post_quirurgica")
+@app.route("/postquirurgico/evaluacion_post_quirurgica")
 @login_required
 def evaluacion_post_quirurgica():
     links = [
             {'name': 'Home', 'url': '/'}, 
             {'name': 'Fase Postquirurgica', 'url': '/postquirurgico'},
-            {'name': 'Evaluacion Post Anestesica', 'url': '/evaluacion_post_quirurgica'}]
+            {'name': 'Evaluacion Post Anestesica', 'url': '/postquirurgico/evaluacion_post_quirurgica'}]
     from models import RutForm, EvaluacionPostquirurgicaForm
     rut_form = RutForm()
     evaluacion_post_quirurgica_form = EvaluacionPostquirurgicaForm()
@@ -191,13 +219,13 @@ def evaluacion_post_quirurgica():
         rut_form=rut_form, evaluacion_post_quirurgica_form=evaluacion_post_quirurgica_form,
         links=links)
 
-@app.route("/coordinar_traslado", methods=['GET', 'POST'])
+@app.route("/postquirurgico/coordinar_traslado", methods=['GET', 'POST'])
 @login_required
 def coordinar_traslado():
     links = [
             {'name': 'Home', 'url': '/'}, 
             {'name': 'Fase Postquirurgica', 'url': '/postquirurgico'},
-            {'name': 'Coordinar Traslado', 'url': '/coordinar_traslado'}]
+            {'name': 'Coordinar Traslado', 'url': '/postquirurgico/coordinar_traslado'}]
     from models import CoordinarTrasladoForm
     coordinar_traslado_form = CoordinarTrasladoForm()
 
@@ -221,29 +249,46 @@ def coordinar_traslado():
         coordinar_traslado_form=coordinar_traslado_form,
         links=links)
 
-@app.route("/estado_paciente", methods=['GET', 'POST'])
+@app.route("/postquirurgico/paciente", methods=['GET', 'POST'])
 @login_required
 def estado_paciente():
     links = [
             {'name': 'Home', 'url': '/'}, 
             {'name': 'Fase Postquirurgica', 'url': '/postquirurgico'},
-            {'name': 'Estado Paciente', 'url': '/estado_paciente'}]
-    from models import RutForm
-    rut_form = RutForm()
-    if request.method == 'POST':
-        rut = rut_form.rut.data
-        paciente = Paciente.query.filter_by(rut=rut).first()
-        return redirect(url_for(f'estado_paciente/{paciente.rut}'))
-    return render_template("estado_paciente.html", rut_form=rut_form, links=links)
+            {'name': 'Estado Paciente', 'url': '/postquirurgico/paciente'}]
+    query = "SELECT * FROM paciente"
+    pacientes = db.engine.execute(query)
+    return render_template("estado_paciente.html", links=links, pacientes=pacientes)
 
-@app.route("/estado_paciente/<rut>", methods=['GET', 'POST'])
+@app.route("/postquirurgico/paciente/<id>", methods=['GET', 'POST'])
 @login_required
-def estado_paciente_rut(rut):
+def estado_paciente_rut(id):
     links = [
             {'name': 'Home', 'url': '/'},
             {'name': 'Fase Postquirurgica', 'url': '/postquirurgico'},
-            {'name': 'Estado Paciente', 'url': '/estado_paciente'}]
-    return render_template("datos_paciente.html", rut=rut, links=links)
+            {'name': 'Estado Paciente', 'url': '/postquirurgico/estado_paciente/'+id}]
+    query = "SELECT * FROM paciente WHERE id = %s" % id
+    paciente = db.engine.execute(query)
+    for row in paciente:
+        pac = row
+    query_evaluaciones = "SELECT * FROM evaluacion WHERE rut_paciente = '%s'" % pac.rut
+    evaluaciones = db.engine.execute(query_evaluaciones)
+    return render_template("datos_paciente.html", pac=pac, evaluaciones = evaluaciones, id=id, links=links)
+
+@app.route("/postquirurgico/paciente/<id>/evaluacion/<id_evaluacion>", methods=['GET', 'POST'])
+@login_required
+def estado_paciente_rut_evaluacion(id, id_evaluacion):
+    links = [
+            {'name': 'Home', 'url': '/'},
+            {'name': 'Fase Postquirurgica', 'url': '/postquirurgico'},
+            {'name': 'Estado Paciente', 'url': '/postquirurgico/paciente/'+id},
+            {'name': 'Evaluacion', 'url': '/postquirurgico/paciente/'+id+'/evaluacion/'+id_evaluacion}]
+
+    query_evaluacion = "SELECT * FROM evaluacion WHERE id = %s" % id_evaluacion
+    evaluacion = db.engine.execute(query_evaluacion)
+    for row in evaluacion:
+        eva = row
+    return render_template("evaluacion.html", id=id, links=links, eva=eva)
 
 if __name__ == '__main__':
     app.run()
